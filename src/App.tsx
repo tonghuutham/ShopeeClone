f119affc3dd429e3bda7569780d06a350e71b3e7
tonghuutham <tonghuutham@gmail.com>
2023-03-16 15:37:19 +0700

    xử lý message với axios Interceptors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import useRouterElements from './useRouterElements'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   const routerElements = useRouterElements()
-  return <div>{routerElements}</div>
+  return (
+    <div>
+      {routerElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App

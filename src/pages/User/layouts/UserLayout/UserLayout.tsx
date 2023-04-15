ee8424aa17a6bb5cc13aab357aa5ad89da408564
tonghuutham <tonghuutham@gmail.com>
2023-04-15 17:29:00 +0700

    Code UI UserSlideNav

--- src/pages/User/layouts/UserLayout/UserLayout.tsx
+++ src/pages/User/layouts/UserLayout/UserLayout.tsx
@@ -4,9 +4,20 @@ import { Outlet } from 'react-router-dom'
 
 export default function UserLayout() {
   return (
-    <div>
-      <UserSideNav />
-      <Outlet />
+    <div className='bg-neutral-100 py-12 text-sm text-gray-600'>
+      <div className='container'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-12'>
+          {/* // <UserSideNav /> */}
+          <div className='md:col-span-3 lg:col-span-2'>
+            <UserSideNav />
+          </div>
+
+          {/* <Outlet /> */}
+          <div className='md:col-span-9 lg:col-span-10'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

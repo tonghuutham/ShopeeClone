5ea7a702b6c9c8de8e077182ca639869f8bb19e0
tonghuutham <tonghuutham@gmail.com>
2023-04-17 09:37:19 +0700

    Khai báo Api Profile

---
+++ src/apis/user.api.ts
@@ -0,0 +1,26 @@
+import { Omit } from 'lodash'
+import { User } from 'src/types/user.type'
+import { SuccessResponse } from 'src/types/utils.type'
+import http from 'src/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'roles' | 'email' | 'updatedAt' | 'createdAt'> {
+  password?: string
+  new_password?: string
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SuccessResponse<User>>('/me')
+  },
+  updateProfile(body: BodyUpdateProfile) {
+    return http.put<SuccessResponse<User>>('/user', body)
+  },
+  uploadAvater(body: FormData) {
+    return http.put('/user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
+export default userApi
